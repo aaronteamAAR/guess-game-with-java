10fda186036da089f9fca591cf4516ac4ed54a45
Aaron Chris <rooneychris83@gmail.com>
2022-06-28 19:41:17 +0100

    guess

---
+++ checker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Guess
+{
+	public static void main(String[] args)
+	{
+		int atempt = 1;
+		int userGuess = 0;
+		int guess = (int) (Math.random() * 99 + 1);
+
+
+		Scanner userin = new Scanner(System.in);
+		System.out.println("Guess and win a prize")
+
+		do{
+			System.out.print("Enter a guess within 1 - 100\n")
+			if()
+		}
+	}
+}
